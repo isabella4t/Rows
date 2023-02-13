cc09c0acf542bf0d8f57e04edc015ca76047c2c7
Izzy T <itang2023@kehillahstudent.org>
2023-02-13 14:57:21 -0800

    "Added gausssian elimination or zeroizing"

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,76 @@
 public class Main {
     public static void main(String[] args) {
 
-    }
-
-    public static double[] row(double[][] equations, double solutions[]){
-        //I am a little confused
-        //Answer to two systems of equations
-        /*
-
-        3x + y = 9
-        -1/4y = -21/4
-
-        we do multiplication
-
-        3x + y = 4
-        y = 5
-        soo
-
-        [-1/3]
-        [5]
-
-
-         */
-
-
-
-        double[] ret = new double[2];
-        /*
-        [3 1]  [4]  []
-        [1 0]  [5]  [5]
-
-         */
-        //I did not make this code abstract
-
-        //It looks for the nonzero and substitutes it up
-        if(equations[1][0]!=0) {
-            //x is filled
-            ret[1] = solutions[1] / equations[1][0];
-            ret[0] = (solutions[0]-(equations[0][0]*ret[1]))/equations[0][1];
-
-        }
-
-        if(equations[1][1]!=0) {
-            // answer to first equation is sol 1 / coeff
-            ret[1] = solutions[1] / equations[1][1];
-            ret[0] = (solutions[0]-(equations[0][1]*ret[1]))/equations[0][0];
-
-        }
-
-        return ret;
+        test1();
 
     }
 
-    public static void backsub(double[][] equations, double solutions[]){
-        double[] x = new double[solutions.length];
+    public static void test1(){
+        double[][] coeffs = {{3,1},{0,2}};
+        double[] answers = {10,2};
+
+        backsub(coeffs,answers);
+    }
+
+//backsub takes a set of coeffs from an equation and the solution to the equation and should return the solutions 'x' and 'y'
+    public static double[] backsub(double[][] equations, double solutions[]){
+        double[] ret = new double[equations[0].length];
+
+        //The code is very not abstract
+
+        //ok so first bottom row find y assuming x=0
+        ret[1] = solutions[1]/equations[1][1];
+
+        //now change top row solution without y
+        solutions[0] -= ret[1]*equations[0][1];
+
+        //now divide solution by the coeff
+
+        solutions[0] /= equations[0][0];
+        //now ret should be okay
+       // return ret;
+
+        for(int b = equations.length-1; b>=0;b--){
+
+        }
+        /*
+        This code was my attempt at abstracting it
         for(int k = solutions.length-1;k>=0;k--){
             for(int i = solutions.length-k;i>0;i--){
-                x[k] = solutions[k]/equations[k][k];
+                ret[k] = solutions[1]/equations[1][k];
             }
 
             //epic equation to get the first number
-            x[k] = solutions[k]/equations[k][k];
+            ret[k] = solutions[k]/equations[k][k];
         }
+
+         */
+    }
+
+    public static void GaussianElim(double[][] equations, double solutions[]){
+        /*
+        [3 1] [x] = 10
+        [1 2] [y] = 5
+
+        3x + 1y = 10
+        1x + 2y = 5
+
+        x + 1/3 y = 10/3
+        0 + 2/3 y = 5/3
+
+         */
+
+        //first multiply top row by bottomleft/topleft
+        double topmultiply = equations[1][0]/equations[0][0];
+        for(int i = 0; i< equations[0].length;i++){
+            equations[0][i] = equations[0][i]*topmultiply;
+        }
+        //now minus the bottom by top which should get rid of x
+
+        for(int i = 0; i<equations[1].length;i++){
+            equations[1][i] = equations[1][i]-equations[0][i];
+        }
+        //now x should be 0
+
     }
 }
