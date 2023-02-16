b605663e39715da1ff58a076682085224fbd0445
Izzy T <itang2023@kehillahstudent.org>
2023-02-16 14:48:35 -0800

    "zeroized test code"

--- src/Main.java
+++ src/Main.java
@@ -2,83 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         test1();
+        testZero();
 
     }
-
-    public static void test1(){
-        double[][] coeffs = {{3,1},{0,2}};
-        double[] answers = {10,2};
-
-        double[] nee = backsub(coeffs,answers);
-        for(int i =0;i<nee.length;i++){
-           System.out.println(" " + nee[i]);
-        }
-    }
-//AHHH this code only takes 2x2 matrices
-//backsub takes a set of coeffs from an equation and the solution
-// to the equation and should return the solutions 'x' and 'y'
-    public static double[] backsub(double[][] equations, double solutions[]){
-        double[] ret = new double[equations[0].length];
-
-        //The code is very not abstract
-
-        //ok so first bottom row find y assuming x=0
-        ret[1] = solutions[1]/equations[1][1];
-
-        //now change top row solution without y
-        solutions[0] -= ret[1]*equations[0][1];
-
-        //now divide solution by the coeff
-
-        solutions[0] /= equations[0][0];
-        //now ret should be okay
-        return ret;
-
-        /*
-        for(int b = equations.length-1; b>=0;b--){
-
-        }
-
-        This code was my attempt at abstracting it
-        for(int k = solutions.length-1;k>=0;k--){
-            for(int i = solutions.length-k;i>0;i--){
-                ret[k] = solutions[1]/equations[1][k];
-            }
-
-            //epic equation to get the first number
-            ret[k] = solutions[k]/equations[k][k];
-        }
-
-         */
-    }
-
-    public static void GaussianElim(double[][] equations, double solutions[]){
-        /*
-        [3 1] [x] = 10
-        [1 2] [y] = 5
-
-        3x + 1y = 10
-        1x + 2y = 5
-
-        x + 1/3 y = 10/3
-        0 + 2/3 y = 5/3
-
-         */
-
-        //first multiply top row by bottomleft/topleft
-        double topmultiply = equations[1][0]/equations[0][0];
-        for(int i = 0; i< equations[0].length;i++){
-            equations[0][i] = equations[0][i]*topmultiply;
-        }
-        //now minus the bottom by top which should get rid of x
-
-        for(int i = 0; i<equations[1].length;i++){
-            equations[1][i] = equations[1][i]-equations[0][i];
-        }
-        //now x should be 0
-
-    }
-
+    //notes about equations
     /*
     [1 1] [1 0] =  [1 1]
     [1 2] [0 1] =  [1 2]
@@ -102,6 +29,106 @@ public class Main {
     x = A^-1 b
 
      */
+
+    public static void test1(){
+        double[][] coeffs = {{3,1},{0,2}};
+        double[] answers = {10,2};
+
+        double[] nee = backsub(coeffs,answers);
+        for(int i =0;i<nee.length;i++){
+           System.out.println(" " + nee[i]);
+        }
+    }
+
+    public static void testZero(){
+        double[][] coeffs = {{3,1},{4,1}};
+        double[] and = {5,6};
+        zeroize(coeffs,and,1);
+        for(int i =0;i<coeffs.length;i++){
+           for(int j = 0; j<coeffs[0].length;j++){
+               System.out.print( "[" + coeffs[i][j]+ " ]");
+           }
+           System.out.println(" = [" + and[i]+ "] \n");
+        }
+    }
+
+//AHHH this code only takes 2x2 matrices
+//backsub takes a set of coeffs from an equation and the solution
+// to the equation and should return the solutions 'x' and 'y'
+
+    public static double[] backsub(double[][] equations, double solutions[]){
+        double[] ret = new double[equations[0].length];
+
+        //The code is very not abstract
+
+        //ok so first bottom row find y assuming x=0
+        ret[1] = solutions[1]/equations[1][1];
+
+        //now change top row solution without y
+        solutions[0] -= ret[1]*equations[0][1];
+
+        //now divide solution by the coeff
+
+        ret[0] = solutions[0]/equations[0][0];
+        //now ret should be okay
+
+
+        return ret;
+
+        /*
+        for(int b = equations.length-1; b>=0;b--){
+
+        }
+
+        This code was my attempt at abstracting it
+        for(int k = solutions.length-1;k>=0;k--){
+            for(int i = solutions.length-k;i>0;i--){
+                ret[k] = solutions[1]/equations[1][k];
+            }
+
+            //epic equation to get the first number
+            ret[k] = solutions[k]/equations[k][k];
+        }
+
+         */
+    }
+//this essentially zeroizes the thing
+    public static void zeroize(double[][] equations, double solutions[], int botRow){
+        /*
+        [3 1] [x] = 10
+        [1 2] [y] = 5
+
+        3x + 1y = 10
+        1x + 2y = 5
+
+        x + 1/3 y = 10/3
+        0 + 2/3 y = 5/3
+
+         */
+
+        //first multiply top row by bottomleft/topleft
+        double topmultiply = equations[botRow][botRow-1]/equations[botRow-1][botRow-1];
+        for(int i = 0; i< equations[botRow-1].length;i++){
+            equations[botRow-1][i] = equations[botRow-1][i]*topmultiply;
+            solutions[botRow-1] *= topmultiply;
+        }
+        //now minus the bottom by top which should get rid of x
+
+        for(int i = 0; i<equations[1].length;i++){
+            equations[botRow][i] = equations[botRow][i]-equations[botRow-1][i];
+            solutions[botRow] = solutions[botRow]- solutions[botRow-1];
+        }
+        //now x should be 0
+
+    }
+
+    //TODO: make a triangularize method to make upper triangle
+
+    public static void triangularize(double[][] equations, double solutions[]){
+        for(int i = equations.length-1; i>=0; i--){
+
+        }
+    }
 
     public static int[][] Matrixmultiply(int[][] fir, int[][] sec){
         //new array is row length in first by column length in second
